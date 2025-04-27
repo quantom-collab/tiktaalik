4d124ec54c4e399e118a0cf2519e96101c250679
Adam Freese <adamfreese@proton.me>
2025-04-27 12:32:23 -0400

    Add to pyproject.toml

--- setup.py
+++ setup.py
@@ -3,11 +3,6 @@ from __future__ import annotations
 from skbuild import setup
 
 setup(
-    name="tiktaalik",
-    version="0.0.3",
-    description="a minimal example package (fortran version)",
-    author="The scikit-build team",
-    license="BSD 3-clause license",
     packages=["f90src", "tiktaalik"],
     cmake_languages=("C", "Fortran"),
     cmake_minimum_required_version="3.18",
