7366b9ac21d8c59f5595d4b2afbc2f3573ed05fd
Adam Freese <maxwellsdemon137@gmail.com>
2025-05-10 11:15:55 -0400

    Minor tweaks to build system

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 from skbuild import setup
 
 setup(
+    name="tiktaalik",
+    version="0.3.0",
     packages=["f90src", "tiktaalik"],
     cmake_languages=("C", "Fortran"),
     cmake_minimum_required_version="3.18",
