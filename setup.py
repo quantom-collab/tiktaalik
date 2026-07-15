3ae9c72e72e976ff019c1988662acdd765bc184c
Adam Freese <adamfreese@proton.me>
2026-07-15 17:11:26 -0400

    New mitigation for scikit-build/scikit-build#521

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from __future__ import annotations
 from skbuild import setup
 import re
 from pathlib import Path
+import shutil
 
 # Mitigation of issue mentioned here:
 # https://github.com/scikit-build/scikit-build/issues/521
-for i in (Path(__file__).resolve().parent / "_skbuild").rglob("CMakeCache.txt"):
-    i.write_text(re.sub("^//.*$\n^[^#].*build-env.*$", "", i.read_text(), flags=re.M))
+# The old mitigation is no longer working, so take a more extreme approach...
+skpath = Path(__file__).resolve().parent / "_skbuild"
+if(skpath.exists()):
+    shutil.rmtree(skpath)
 
 setup(
     name="tiktaalik",
